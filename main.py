314642fd8a3349a70dd8587ef4e09b391907841f
James Chao <jc7829@yahoo.com>
2022-04-29 20:53:35 -0700

    init

---
+++ main.py
@@ -0,0 +1,62 @@
+import logging
+import time
+import coloredlogs
+
+from Coach import Coach
+from othello.OthelloGame import OthelloGame as Game
+from othello.tensorflow2.NNet import NNetWrapper as nn
+from utils import *
+
+log = logging.getLogger(__name__)
+
+coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
+
+args = dotdict({
+    'numIters':75,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 15,        #
+    'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
+    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 1,
+
+    'checkpoint': './temp3/',
+    'load_model': True,
+    'load_folder_file': ('./temp2/','best'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+
+def main():
+    log.info('Loading %s...', Game.__name__)
+    g = Game(8)
+
+    log.info('Loading %s...', nn.__name__)
+    nnet = nn(g)
+
+    if args.load_model:
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file[0], args.load_folder_file[1])
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+    else:
+        log.warning('Not loading a checkpoint!')
+
+    log.info('Loading the Coach...')
+    c = Coach(g, nnet, args)
+
+    if args.load_model:
+        log.info("Loading 'trainExamples' from file...")
+        c.loadTrainExamples()
+
+    log.info('Starting the learning process 🎉')
+
+    start = time.time()
+    c.learn()
+    stop = time.time()
+    print(f"Training time: {stop - start}s")
+
+
+
+if __name__ == "__main__":
+    main()
